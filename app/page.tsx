e7e753637fa89cd1865705e6b497671433b1cbf0
Luke Oliff <luke@lukeoliff.com>
2023-11-09 15:54:17 +0000

    fix: fix incorrect url

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default async function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://github.com/deepgram-starters"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
