5eb9ecb853a2cb0c8f23968ac6ffc4a69f71ad56
joeBlockchain <josephataylor@gmail.com>
2024-02-02 12:13:04 -0800

    fixed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Live transcription by Deepgram in Next.js</title>
+        <title>AI Note Taker</title>
       </head>
       <body className={inter.className}>
         <Providers>
