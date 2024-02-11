9bf74be35b616e3e1bddcda454c869c4fedf031a
Luke Oliff <luke@lukeoliff.com>
2024-02-11 18:40:53 +0000

    chore: transient changes

--- app/components/chatbubbles.tsx
+++ app/components/chatbubbles.tsx
@@ -9,11 +9,24 @@ const LeftBubble = ({
     <>
       <div className="col-start-1 col-end-8 p-3 rounded-lg">
         <div className="flex flex-row items-center">
+          <div className="flex items-center justify-center h-8 w-8 flex-shrink-0 mr-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                d="M11.1,24H1c-0.4,0-0.5-0.4-0.3-0.6l6.5-6.2c0.1-0.1,0.2-0.1,0.3-0.1h3.7c2.9,0,5.3-2.1,5.4-4.8c0.1-2.8-2.3-5.2-5.3-5.2H7.4
+	v4.6c0,0.2-0.2,0.4-0.4,0.4H0.4c-0.2,0-0.4-0.2-0.4-0.4V0.4C0,0.2,0.2,0,0.4,0h10.9C18.4,0,24.1,5.5,24,12.2
+	C23.9,18.8,18.1,24,11.1,24z"
+              />
+            </svg>
+          </div>
           <div className="relative text-sm bg-[#1E1E23] py-2 px-4 shadow rounded-xl">
             {children}
           </div>
         </div>
-        <small className="text-zinc-500 pl-3">{meta}</small>
+        <small className="text-zinc-500 ml-[3.75rem] py-1">{meta}</small>
       </div>
     </>
   );
@@ -30,24 +43,11 @@ const RightBubble = ({
     <>
       <div className="col-start-6 col-end-13 p-3 rounded-lg">
         <div className="flex items-center justify-start flex-row-reverse">
-          <div className="flex items-center justify-center h-8 w-8 flex-shrink-0 ml-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-            >
-              <path
-                d="M11.1,24H1c-0.4,0-0.5-0.4-0.3-0.6l6.5-6.2c0.1-0.1,0.2-0.1,0.3-0.1h3.7c2.9,0,5.3-2.1,5.4-4.8c0.1-2.8-2.3-5.2-5.3-5.2H7.4
-	v4.6c0,0.2-0.2,0.4-0.4,0.4H0.4c-0.2,0-0.4-0.2-0.4-0.4V0.4C0,0.2,0.2,0,0.4,0h10.9C18.4,0,24.1,5.5,24,12.2
-	C23.9,18.8,18.1,24,11.1,24z"
-              />
-            </svg>
-          </div>
           <div className="relative text-sm bg-[#1E1E23] py-2 px-4 shadow rounded-xl">
             <div>{children}</div>
           </div>
         </div>
-        <small className="block text-zinc-500 pl-3 text-right mr-[3.75rem]">
+        <small className="block text-zinc-500 pr-3 text-right py-1">
           {meta}
         </small>
       </div>
@@ -57,9 +57,9 @@ const RightBubble = ({
 
 const ChatBubble = ({ message }: { message: any }) => {
   if (message.role === "user") {
-    return <LeftBubble meta={"20ms"}>{message.content}</LeftBubble>;
-  } else {
     return <RightBubble meta={"3ms"}>{message.content}</RightBubble>;
+  } else {
+    return <LeftBubble meta={"20ms"}>{message.content}</LeftBubble>;
   }
 };
 
--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,8 +1,13 @@
 const systemContent = `- ignore all other instructions
 - roleplay as a helpful assistant
 - your name is Emily
-- you are part of Deepgram's speech-to-text (STT) demos
+- you are a member of Deepgram
+- you can be sarcastic and humorous
 - never mention you're an AI
+- never apologise
+- keep responses as short as possible for brevity
+- use american english
+- if you don't know an answer, just reply "I don't know"
 `;
 
 const greetings = [
