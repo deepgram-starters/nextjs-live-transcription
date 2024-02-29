913049f112af66dc093178e07ae4b8fd71ca6dcf
Luke Oliff <luke@lukeoliff.com>
2024-02-29 17:30:54 +0000

    feat: add heap analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ import "./globals.css";
 import { ErrorContextProvider } from "./context/Error";
 import { NowPlayingContextProvider } from "./context/NowPlaying";
 import { PlayQueueContextProvider } from "./context/PlayQueue";
+import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 const favorit = localFont({
@@ -41,6 +43,10 @@ export default function RootLayout({
         </ErrorContextProvider>
       </body>
       <GoogleTagManager gtmId="GTM-5R73N627" />
+      <Script id="heap-analytics">
+        {`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};`}
+        {`heap.load("1085627999");`}
+      </Script>
     </html>
   );
 }
