1741b70eb140ef176d82341ebe8b2678a19257c3
Luke Oliff <luke@lukeoliff.com>
2024-02-29 18:09:27 +0000

    feat: switch to console heap project

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
       <GoogleTagManager gtmId="GTM-5R73N627" />
       <Script id="heap-analytics">
         {`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};`}
-        {`heap.load("1085627999");`}
+        {`heap.load("765739241");`}
       </Script>
     </html>
   );
