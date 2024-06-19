28f83c457bde07e23c737a95bb02f29a8dc97b0b
Joey Xiong <junruxiong@gmail.com>
2024-06-19 14:14:59 +0100

    Add some charts to dashboard

--- frontend-ts/app/dashboard/page.tsx
+++ frontend-ts/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
-"use client";
-
 import Charts from "@/components/Charts";
 
 export default function Page() {
-    return <div />;
+  return (
+    <div className="">
+      <Charts selectedUser={null} selectedToy={null} />
+    </div>
+  );
 }
